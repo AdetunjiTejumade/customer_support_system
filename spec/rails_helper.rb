7645ee69a6d00ea1db67adae724638e620ff1e4d
Tejumade Adetunji <tejumadeadetunji@gmail.com>
2022-07-20 12:16:08 +0100

    fix robocop errors

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require 'database_cleaner/active_record'
@@ -5,7 +7,7 @@ require 'database_cleaner/active_record'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -34,7 +36,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
-   with.test_framework :rspec
+    with.test_framework :rspec
     with.library :rails
   end
 end
